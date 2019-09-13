8e46e1b5376bad98665446f474293baf02993b57
Juan Salazar <j.c.salazar13@gmail.com>
2019-09-13 18:47:21 -0400

    first commit

---
+++ src/pages/Home/index.js
@@ -0,0 +1,25 @@
+import React from "react";
+import { Container, Card, Grid } from '@material-ui/core';
+
+export default () => { 
+	return(
+		<Container>
+			<Card>
+				<Grid container>
+					<Grid>
+
+					</Grid>
+					<Grid>
+						
+					</Grid>
+					<Grid>
+						
+					</Grid>
+					<Grid>
+						
+					</Grid>
+				</Grid>
+			</Card>			
+		</Container>
+	)
+};
